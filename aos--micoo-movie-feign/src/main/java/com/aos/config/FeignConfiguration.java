4f95dd88d4394b2b495f4ebcde0c8907d7ed9f38
lindont <coovoo@vip.qq.com>
2018-01-28 23:03:29 +0800

    change 5

--- aos--micoo-movie-feign/src/main/java/com/aos/config/FeignConfiguration.java
+++ aos--micoo-movie-feign/src/main/java/com/aos/config/FeignConfiguration.java
@@ -2,6 +2,7 @@ package com.aos.config;
 
 import feign.Contract;
 import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +19,12 @@ public class FeignConfiguration {
     }
 
     @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("user", "aosco");
+    }
+
+    @Bean
     Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
+        return Logger.Level.BASIC;
     }
 }
