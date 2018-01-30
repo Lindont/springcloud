618df1c54cd949d75f231216de17bc4a477b5242
lindont <coovoo@vip.qq.com>
2018-01-30 22:52:56 +0800

    change 6

---
+++ aos-spring-eureka-hight/src/main/java/com/aos/cloud/HightEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.aos.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HightEurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaApplication.class, args);
+	}
+}
